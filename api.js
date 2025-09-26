b3c280c98643e3314b1b931225e5c576b5772ebc
By Vannn <farizrevano5@gmail.com>
2025-09-26 17:10:37 +0700

    Delete api.js

--- api.js
+++
@@ -1,5 +0,0 @@
-async function handleBugSend(chatId, selectedBug) {
-  const res = await fetch(`https://Domain/kipopapi?chatId=${encodeURIComponent(chatId)}&type=${selectedBug}`);
-  if (!res.ok) throw new Error("Gagal menghubungi server");
-  return await res.json();
-}
